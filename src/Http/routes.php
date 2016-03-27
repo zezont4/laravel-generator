b1492cfee746c8e19fda1fb0164f3ac0448340dd
zezont4 <zezont@gmail.com>
2016-03-27 06:52:45 +0300

    add home route

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,6 @@
 <?php
 Route::group(['prefix' => 'laravel_generator','middleware' => ['web']], function () {
+    Route::get('/', 'LaravelGeneratorController@showTables');
     Route::get('show_tables', 'LaravelGeneratorController@showTables');
     Route::get('{table}/show_fields', 'LaravelGeneratorController@showFields');
     Route::post('post_to_session', 'LaravelGeneratorController@postToSession');
