d9142e0101128fb7199d6934b77a373d9d2088b8
zezont4 <zezont@gmail.com>
2016-03-29 19:22:59 +0300

    add mkdir

--- src/resources/views/layouts/write_view_to_file.blade.php
+++ src/resources/views/layouts/write_view_to_file.blade.php
@@ -1,8 +1,11 @@
-
 @if(file_exists(base_path()."/resources/views/".strtolower($model_name)."/{$page}.blade.php"))
-    <span class="alert yellow lighten-1 md-size-font">File already exists. for protection, copy it manually</span>
+    <span class = "alert yellow lighten-1 md-size-font">File already exists. for protection, copy it manually</span>
 @else
+    @if(!is_dir(base_path()."/resources/views/".strtolower($model_name)))
+        @if(mkdir(base_path()."/resources/views/".strtolower($model_name)))
+        @endif
+    @endif
     @if(file_put_contents(base_path()."/resources/views/".strtolower($model_name)."/{$page}.blade.php", $text_content))
-        <span class="green-text md-size-font">File Created Successfully</span>
+        <span class = "green-text md-size-font">File Created Successfully</span>
     @endif
 @endif
