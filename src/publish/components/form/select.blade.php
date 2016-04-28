efb8491696941ef3702c7e45b23f3fe401d074a9
zezont4 <zezont@gmail.com>
2016-04-28 16:53:32 +0300

    fix selected valud in select component

--- src/publish/components/form/select.blade.php
+++ src/publish/components/form/select.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="input-field col s12">
-        {{ \Form::select($name, $values, $key==$selected_value ? true : false, array_merge(['class' =>  $errors->has($name) ? 'validate invalid' : '','placeholder' => 'اختر من القائمة ...'], $attributes))}}
+        {{ \Form::select($name, $values, $selected_value , array_merge(['class' =>  $errors->has($name) ? 'validate invalid' : '','placeholder' => 'اختر من القائمة ...'], $attributes))}}
         {{ \Form::label($name, $label, $errors->has($name) ? ['data-error' => $errors->first($name)]:null) }}
     </div>
 </div>
