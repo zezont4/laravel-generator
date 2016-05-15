a1b925be436ee320c049c158b5241e0514b6c84e
Abdulaziz Altayyar <zezont@gmail.com>
2016-05-15 13:47:10 +0300

    change radio,check to inline

--- src/publish/components/form/check.blade.php
+++ src/publish/components/form/check.blade.php
@@ -2,10 +2,10 @@
     <div class="radio_rapper col s12">
         <p class="title-font {{$errors->has($name) ? 'validate invalid' : ''}} {{isset($attributes['is_required']) ? 'required' : ''}}" data-error="{{$errors->first($name)}}">{{$label}}</p>
         @foreach ($values as $key => $value)
-            <p>
+            <span>
                 {{ \Form::CheckBox($name, $key, $key===$selected_value ? true : false,array_merge($attributes,['id'=>$name.$key]))}}
                 {{ \Form::label($name.$key, $value) }}
-            </p>
+            </span>
         @endforeach
     </div>
 </div>
