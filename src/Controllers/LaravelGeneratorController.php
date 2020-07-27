a552d93c3bb558a0e1318eff5c74bd9d44736c0f
zezont4 <zezont@gmail.com>
2020-07-27 18:49:18 +0300

    add primary key variable

--- src/Controllers/LaravelGeneratorController.php
+++ src/Controllers/LaravelGeneratorController.php
@@ -15,7 +15,7 @@ class LaravelGeneratorController extends Controller
 
         $views = DB::select('SHOW FULL TABLES where Table_type="VIEW"');
 
-        return view('package_views::generator.show_tables', \compact('tables', 'views', 'field_name_for_table_name'));
+        return view('package_views::generator.show_tables', compact('tables', 'views', 'field_name_for_table_name'));
     }
 
     public function showFields($table)
@@ -24,7 +24,7 @@ class LaravelGeneratorController extends Controller
 
         $fields = DB::select("SHOW FULL COLUMNS FROM {$table}");
         //dd($fields);
-        return view('package_views::generator.show_fields', \compact('fields'));
+        return view('package_views::generator.show_fields', compact('fields'));
     }
 
     public function postToSession(Request $request)
@@ -46,27 +46,29 @@ class LaravelGeneratorController extends Controller
         $table_label = $request->filled('table_label') ? $request->table_label : \ucfirst($table_name);
 
         $fields_array = [];
+        $primary_key = 'id';
         if (session()->has('fields')) {
             foreach (session('fields')['field_name'] as $key => $field) {
                 if (session('fields')['key'][$key] == 'PRI') {
                     session()->put('primary_key', $field);
+                    $primary_key=$field;
                 }
 
                 $label = session('fields')['field_comment'][$key];
 
-                \array_push($fields_array, [
+                array_push($fields_array, [
                     'name'        => $field,
                     'label'       => $label ? $label : \ucwords(\str_replace('_', ' ', $field)),
                     'type'        => session('fields')['input_type'][$key],
                     'data_type'   => session('fields')['field_type'][$key],
                     'key'         => session('fields')['key'][$key],
-                    'is_selected' => \in_array($field, session('fields')['select']),
-                    'is_required' => \in_array($field, session('fields')['required']),
+                    'is_selected' => in_array($field, session('fields')['select']),
+                    'is_required' => in_array($field, session('fields')['required']),
                 ]);
             }
         }
         session()->put('fields_array', $fields_array);
         //		dd(session('fields_array'));
-        return view('package_views::generator.make_pages', \compact('fields_array', 'table_name', 'primary_key', 'model_name', 'table_label'));
+        return view('package_views::generator.make_pages', compact('fields_array', 'table_name', 'primary_key', 'model_name', 'table_label'));
     }
 }
--- src/LaravelGeneratorServiceProvider.php
+++ src/LaravelGeneratorServiceProvider.php
@@ -55,39 +55,39 @@ class LaravelGeneratorServiceProvider extends ServiceProvider
 
     public function loadZezont4Components()
     {
-        \Form::component('mtText', 'zezont4.components.form.text', ['name', 'label', 'value' => null, 'attributes' => []]);
-        \Form::component('mtTel', 'zezont4.components.form.tel', ['name', 'label', 'value' => null, 'attributes' => []]);
-        \Form::component('mtPassword', 'zezont4.components.form.password', ['name', 'label', 'attributes' => []]);
+        Form::component('mtText', 'zezont4.components.form.text', ['name', 'label', 'value' => null, 'attributes' => []]);
+        Form::component('mtTel', 'zezont4.components.form.tel', ['name', 'label', 'value' => null, 'attributes' => []]);
+        Form::component('mtPassword', 'zezont4.components.form.password', ['name', 'label', 'attributes' => []]);
 
-        \Form::component('mtDate', 'zezont4.components.form.date', ['name', 'label', 'value' => null, 'attributes' => []]);
+        Form::component('mtDate', 'zezont4.components.form.date', ['name', 'label', 'value' => null, 'attributes' => []]);
 
-        \Form::component('mtRadio', 'zezont4.components.form.radio', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
-        \Form::component('mtCheck', 'zezont4.components.form.check', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
-        \Form::component('mtSelect', 'zezont4.components.form.select', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
-        \Form::component('mtDefaultSelect', 'zezont4.components.form.default_select', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
+        Form::component('mtRadio', 'zezont4.components.form.radio', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
+        Form::component('mtCheck', 'zezont4.components.form.check', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
+        Form::component('mtSelect', 'zezont4.components.form.select', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
+        Form::component('mtDefaultSelect', 'zezont4.components.form.default_select', ['name', 'label', 'selected_value' => null, 'values', 'attributes' => []]);
 
-        \Form::component('mtButton', 'zezont4.components.form.button', ['label', 'class' => 'waves-light btn']);
+        Form::component('mtButton', 'zezont4.components.form.button', ['label', 'class' => 'waves-light btn']);
 
-        \Form::component('mtStatic', 'zezont4.components.form.static', ['label', 'value']);
+        Form::component('mtStatic', 'zezont4.components.form.static', ['label', 'value']);
     }
 
     public function loadZezont4BladeDirectives()
     {
-        \Blade::directive('hasRole', function ($role_slug) {
+        Blade::directive('hasRole', function ($role_slug) {
             return "<?php if (auth()->check()) :
 				if (auth()->user()->hasRole({$role_slug})) : ?>";
         });
 
-        \Blade::directive('endhasRole', function () {
+        Blade::directive('endhasRole', function () {
             return '<?php endif; endif; ?>';
         });
 
-        \Blade::directive('hasPermission', function ($permission_slug) {
+        Blade::directive('hasPermission', function ($permission_slug) {
             return "<?php if (auth()->check()) :
 				if (auth()->user()->allowedTo({$permission_slug})) : ?>";
         });
 
-        \Blade::directive('endhasPermission', function () {
+        Blade::directive('endhasPermission', function () {
             return '<?php endif; endif; ?>';
         });
     }
