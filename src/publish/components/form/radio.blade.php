c1bf103c147f37937876972e3c4399956f591697
Abdulaziz Altayyar <zezont@gmail.com>
2023-05-05 05:38:13 +0300

    Update radio.blade.php

--- src/publish/components/form/radio.blade.php
+++ src/publish/components/form/radio.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="radio_rapper col s12">
-        <p class="title-font {{$errors->has($name) ? 'validate invalid' : ''}} {{isset($attributes['is_required']) ? 'required' : ''}}" data-error="{{$errors->first($name)}}">{{$label}}</p>
+        <p class="title-font {{$errors->has($name) ? 'validate invalid' : ''}} {{isset($attributes['is_required']) ? 'required' : ''}}" data-error="{{$errors->first($name)}}">{!! $label !!}</p>
         @foreach ($values as $key => $value)
             <span>
                 {{ \Form::radio($name, $key, $key===$selected_value ? true : false,array_merge($attributes,['id'=>$name.$key]))}}
